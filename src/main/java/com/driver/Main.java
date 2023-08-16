29eddaeb5aeb5c5eaccd36f88a8d6833a3dc85b4
Your Name <you@example.com>
2023-08-16 12:22:09 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
 
+        int result1 = p.product(3, 5);
+        System.out.println(result1);
+
+        int result2 = p.product(2, 4, 6);
+        System.out.println(result2);
+
+        int result3 = p.product(2, 4, 6,8);
+        System.out.println(result3);
+    }
+static class Product{
+    public int product(int x, int y) {
+        return x * y;
+    }
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    public int product(int x, int y, int z, int f) {
+        return x * y * z * f;
+        }
+     }
 }
